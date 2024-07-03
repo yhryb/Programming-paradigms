8e3558a8aeb862200cabf2e7e9250c7c59e56cff
yhryb <yhryb@kse.org.ua>
2024-07-03 21:33:01 +0300

    Assignment 4 Milestone 1

--- TextEncryption/CaesarCipher.cpp
+++ TextEncryption/CaesarCipher.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "CaesarCipher.h"
+#include <fstream>
 
 CaesarCipher::CaesarCipher() {
     handle = dlopen("./libcipher.dylib", RTLD_LAZY);
@@ -10,7 +11,7 @@ CaesarCipher::CaesarCipher() {
         fprintf(stderr, "Error: %s\n", dlerror());
         exit(1);
     }
-    loadFunctionPointer((void*&)encryptFunction, "encrypt");
+    loadFunctionPointer((void*&)encryptFunction, "encrypt"); //loading function pointer
     loadFunctionPointer((void*&)decryptFunction, "decrypt");
 }
 CaesarCipher::~CaesarCipher() {
@@ -29,4 +30,66 @@ void CaesarCipher::loadFunctionPointer(void*& function, const char* functionName
 
 char* CaesarCipher::encrypt(const char* rawText, int key) {
     return encryptFunction(rawText, key);
+}
+
+char* CaesarCipher::decrypt(const char* encryptedText, int key) {
+    return decryptFunction(encryptedText, key);
+}
+
+void CaesarCipher::encryptFile(const char* inputFilePath, const char* outputFilePath, int key) { //encrypting file
+    std::ifstream inputFile(inputFilePath, std::ios::binary); //binary to allow encrypting byte sequences of any size
+    if (!inputFile.is_open()) {
+        std::cerr << "Failed to open input file: " << inputFilePath;
+    }
+    std::ofstream outputFile(outputFilePath, std::ios::binary);
+    if (!outputFile.is_open()) {
+        std::cerr << "Failed to open output file: " << outputFilePath;
+        inputFile.close();
+    }
+    inputFile.seekg(0, std::ios::end); //moves the reading position to the end of the file to calculate its size
+    std::streamsize size = inputFile.tellg(); //gets size of the file
+    inputFile.seekg(0, std::ios::beg); //moves to the beginning
+    char* fileBuffer = new char[size]; //allocates memory for size of the file
+    if (inputFile.read(fileBuffer, size)) {
+        char* encryptedBuffer = encrypt(fileBuffer, key); //encrypting the file
+        outputFile.write(encryptedBuffer, strlen(encryptedBuffer)); //writing encrypted content into the output file
+        delete[] encryptedBuffer;
+    } else {
+        std::cerr << "Failed to read input file: " << inputFilePath;
+        delete[] fileBuffer;
+        inputFile.close();
+        outputFile.close();
+    }
+    delete[] fileBuffer;
+    inputFile.close();
+    outputFile.close();
+}
+
+void CaesarCipher::decryptFile(const char* inputFilePath, const char* outputFilePath, int key) {
+    std::ifstream inputFile(inputFilePath, std::ios::binary);
+    if (!inputFile.is_open()) {
+        std::cerr << "Failed to open input file: " << inputFilePath;
+    }
+    std::ofstream outputFile(outputFilePath, std::ios::binary);
+    if (!outputFile.is_open()) {
+        std::cerr << "Failed to open output file: " << outputFilePath;
+        inputFile.close();
+    }
+    inputFile.seekg(0, std::ios::end);
+    std::streamsize size = inputFile.tellg();
+    inputFile.seekg(0, std::ios::beg);
+    char* fileBuffer = new char[size];
+    if (inputFile.read(fileBuffer, size)) {
+        char* decryptedBuffer = decrypt(fileBuffer, key);
+        outputFile.write(decryptedBuffer, strlen(decryptedBuffer));
+        delete[] decryptedBuffer;
+    } else {
+        std::cerr << "Failed to read input file: " << inputFilePath;
+        delete[] fileBuffer;
+        inputFile.close();
+        outputFile.close();
+    }
+    delete[] fileBuffer;
+    inputFile.close();
+    outputFile.close();
 }
--- TextEncryption/CaesarCipher.h
+++ TextEncryption/CaesarCipher.h
@@ -1,23 +1,25 @@
-#include <dlfcn.h> // Header for dl functions
+#include <dlfcn.h>
 #include <iostream>
 #ifndef CAESARCIPHER_H
 #define CAESARCIPHER_H
-typedef char* (*encrypt_t)(const char*, int);
+typedef char* (*encrypt_t)(const char*, int); //define function pointer types
 typedef char* (*decrypt_t)(const char*, int);
 
 
 class CaesarCipher {
 private:
-    void* handle;
-    encrypt_t encryptFunction;
+    void* handle; //pointer to the dynamic library
+    encrypt_t encryptFunction; //function pointer of type encrypt_t
     decrypt_t decryptFunction;
-    void loadFunctionPointer(void*& function, const char* functionName);
+    void loadFunctionPointer(void*& function, const char* functionName); //method to load a function pointer
 
 public:
     CaesarCipher(); //initialise constructor that loads the library
     ~CaesarCipher(); //initialise desructor that unloads the library
     char* encrypt(const char* rawText, int key);
     char* decrypt(const char* encryptedText, int key);
+    void encryptFile(const char* inputFilePath, const char* outputFilePath, int key);
+    void decryptFile(const char* inputFilePath, const char* outputFilePath, int key);
 };
 
 
